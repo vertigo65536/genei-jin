2f9be9b1dad7f8bab30d122c6c3335d084ee7160
david <david.the.nerd1@gmail.com>
2020-08-18 21:35:40 +0100

    added installer file

---
+++ installer.sh
@@ -0,0 +1,10 @@
+pip install discord --user
+pip install wikipedia --user
+pip install google_auth_oauthlib --user
+pip install google-api-python-client --user
+pip install you --user
+pip install pokedex.py --user
+pip install google_images_download --user
+pip install python-dotenv --user
+touch .env
+printf "DISCORD_TOKEN=\nYOUTUBE_API_KEY=\n" > .env
