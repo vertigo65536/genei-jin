9fc7cfba866c7ebcb8cc858e5b7f24a70d342bbb
david <david.the.nerd1@gmail.com>
2020-11-26 02:12:16 +0000

    updated the installer

--- installer.sh
+++ installer.sh
@@ -1,10 +1,10 @@
 pip install discord --user
 pip install wikipedia --user
-pip install google_auth_oauthlib --user
-pip install google-api-python-client --user
+#pip install google_auth_oauthlib --user
+#pip install google-api-python-client --user
 pip install you --user
 pip install pokedex.py --user
-#pip install google_images_download --user
 pip install python-dotenv --user
+pip install --user currencyconverter
 touch .env
 printf "DISCORD_TOKEN=\nADMIN_ID=\n" > .env
